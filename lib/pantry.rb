c6195bc3901151bdf1c4dd31eb9ca029d8bdfba1
adamgunther1 <adamgunther1@gmail.com>
2017-04-18 09:50:29 -0600

    completes iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,28 @@
+require_relative 'recipe'
+
 class Pantry
+
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if stock[item].nil?
+      return 0
+    else
+      stock[item]
+    end
+  end
+
+  def restock(item, quantity)
+    if stock[item].nil?
+      stock[item] = quantity
+    else
+      exisiting_quantity = stock[item]
+      stock[item] = exisiting_quantity + quantity
+    end
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,47 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def setup
+    @p = Pantry.new
+    @r = Recipe.new("Cheese Pizza")
+  end
+  
+  def test_pantry_exists
+    
+    assert_instance_of Pantry, @p
+  end
+
+  def test_empty_pantry_when_initialized
+    stock = {}
+    assert_equal stock, @p.stock
+  end
+
+  def test_checking_stock_method_before_restocking
+    assert_equal 0, @p.stock_check("Cheese")
+  end
+  
+  def test_restocking
+    @p.restock("Cheese", 10)
+    assert_equal 10, @p.stock_check("Cheese")
+    @p.restock("Cheese", 20)
+    assert_equal 30, @p.stock_check("Cheese")
+  end
+
+  def test_new_recipe_feature_for_pantry
+    ingredient_list = {}
+    assert_equal ingredient_list, @r.ingredients
+    @r.add_ingredient("Cheese", 20)
+    @r.add_ingredient("Flour", 20)
+    ingredient_list = {"Cheese" => 20, "Flour" => 20}
+    assert_equal ingredient_list, @r.ingredients
+  end
+
+  def test_adding_the_recipe_to_the_shopping_list
+    @p.add_to_shopping_list(@r)
+  end
+
+
+
+
+
 end
